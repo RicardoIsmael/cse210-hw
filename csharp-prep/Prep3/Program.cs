43b97da9f05bbdf0f9eb11f97d8a37f7f89116e8
RicardoIsmael <rirojas1970@gmail.com>
2023-01-09 23:03:49 -0300

    prep3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Do you like to play a guess number");
+        Random gran = new Random();
+        int n2 = gran.Next(1, 100);
+        int count = 1;
+        while (true)
+        {
+            Console.Write("choose a number:  ");
+            string num = Console.ReadLine();
+            int n1 = int.Parse(num);
+
+
+            if (n1 < n2)
+            {
+                Console.WriteLine("the number it's Higuer");
+            }
+            else if (n1 > n2)
+            {
+                Console.WriteLine("the number it's Lower");
+            }
+            else
+            {
+                Console.WriteLine("you win");
+                Console.WriteLine($"you have guessed after {count} intents. ");
+                Console.Write("Do you like to play again (enter y/n): ");
+                string op = Console.ReadLine();
+                if (op == "y")
+                {
+                    n2 = gran.Next(1, 100);
+                    count = 1;
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            count += 1;
+        }
+
+
+
+
     }
 }
